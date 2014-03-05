d2a9015779a329887c20ef346c6e50af99a2982d
Peter Smit <peter@smitmail.eu>
2014-03-05 19:53:33 +0200

    Revert "Test c++11 features"
    This reverts commit 9006337e13886b2afcbc37461dc1a51483723876.

--- bot.cc
+++ bot.cc
@@ -1,20 +1,8 @@
 #include "bot.h"
 
-
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 
 int main( int argc, const char* argv[] )
 {
-    std::cout << "No move" << std::endl;
-
-    std::vector<int> v;
-
-    v.push_back(3);
-    v.push_back(5);
-
-    for (auto itr = v.begin(); itr != v.end(); ++itr) {
-        std::cout << "No move" << std::endl;
-    }
-
+	printf( "\nHello World\n\n" );
 }
