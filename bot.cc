87dbb06fbadb6a44d2de3b1aa3bddd7527ae2d64
Peter Smit <peter@smitmail.eu>
2014-03-05 15:18:32 +0200

    Test c++11 features

--- bot.cc
+++ bot.cc
@@ -1,8 +1,20 @@
 #include "bot.h"
 
-#include <stdio.h>
+
+#include <iostream>
+#include <vector>
 
 int main( int argc, const char* argv[] )
 {
-	printf( "\nHello World\n\n" );
+    std::cout << "No move" << std::endl;
+
+    std::vector<int> v;
+
+    v.push_back(3);
+    v.push_back(5);
+
+    for (auto itr = v.begin(); itr != v.end(); ++itr) {
+        std::cout << "No move" << std::endl;
+    }
+
 }
