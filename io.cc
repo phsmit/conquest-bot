c75eb83421053ea9fda8c63c85a3e2eaabf9c7dd
Peter Smit <peter@smitmail.eu>
2014-03-14 11:35:04 +0200

    Implementation of parse_moves

--- io.cc
+++ io.cc
@@ -20,7 +20,7 @@ SettingVector read_pair_list(str data) {
   SettingVector result;
   reg_t k, v;
   while (stream >> k >> v) {
-    result.push_back(Setting(k, v));
+    result.push_back({k, v});
   }
   return result;
 }
@@ -38,8 +38,7 @@ ConnectionVector read_neighbour_list(str data) {
       neighbours.push_back(n);
       if (ss2.peek() == ',') ss2.ignore();
     }
-    Connection c = {k, neighbours};
-    result.push_back(c);
+    result.push_back({k, neighbours});
   }
   return result;
 }
@@ -180,8 +179,8 @@ void IOManager::run_game_loop(StrategyManager &manager) {
 }
 
 void IOManager::write_picks(RegionVector regions) {
-  for (RegionVector::iterator it = regions.begin(); it != regions.end(); ++it) {
-    std::cout << region_id[*it] << " ";
+  for (auto region : regions) {
+    std::cout << region_id[region] << " ";
   }
   std::cout << std::endl;
 }
@@ -192,8 +191,8 @@ void IOManager::write_moves(MoveVector moves) {
     return;
   }
 
-  for (MoveVector::iterator it = moves.begin(); it != moves.end(); ++it) {
-    std::cout << game_setup.name_me << " attack/transfer " << region_id[it->from] << " " << region_id[it->to] << " " << it->amount << ", ";
+  for (auto move : moves) {
+    std::cout << game_setup.name_me << " attack/transfer " << region_id[move.from] << " " << region_id[move.to] << " " << move.amount << ", ";
   }
 
   std::cout << std::endl;
@@ -203,9 +202,9 @@ void IOManager::write_moves(MoveVector moves) {
 void IOManager::write_placements(PlacementVector placements) {
   bool output_written = false;
 
-  for (PlacementVector::iterator it = placements.begin(); it != placements.end(); ++it) {
-    if (it->amount > 0) {
-      std::cout << game_setup.name_me << " place_armies " << region_id[it->to] << " " << it->amount << ", ";
+  for (auto placement : placements) {
+    if (placement.amount > 0) {
+      std::cout << game_setup.name_me << " place_armies " << region_id[placement.to] << " " << placement.amount << ", ";
       output_written = true;
     }
   }
@@ -215,8 +214,30 @@ void IOManager::write_placements(PlacementVector placements) {
 }
 
 MoveVector IOManager::parse_moves(str data) {
-  //TODO: implement
-  return std::vector<Move>();
+  std::map<reg_t, reg_t> map = make_map(region_id);
+
+  MoveVector moves;
+  std::stringstream stream(data);
+
+  str player;
+  str action;
+  reg_t from;
+  reg_t to;
+  army_t amount;
+
+  while (stream >> player >> action) {
+    if (action == "place_armies") {
+      stream >> to >> amount;
+      from = to;
+    } else {
+      stream >> from >> to >> amount;
+    }
+
+    moves.push_back({map[from], map[to], amount});
+  }
+
+
+  return moves;
 }
 
 UpdateVector IOManager::parse_updates(str data) {
@@ -234,8 +255,7 @@ UpdateVector IOManager::parse_updates(str data) {
   army_t amount;
   str player;
   while (stream >> region >> player >> amount) {
-    Update u = {map[region], owner_map[player], amount};
-    result.push_back(u);
+    result.push_back({map[region], owner_map[player], amount});
   }
 
   return result;
