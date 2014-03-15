cc2ff44179b5a3047c9a7fc887cc1d63162139e7
Peter Smit <peter@smitmail.eu>
2014-03-15 09:19:25 +0200

    Always use explicit python2

--- generate_submission.sh
+++ generate_submission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p gen
-python scripts/write_winchance_table.py > gen/nbinom-cdf-gen.h
-python scripts/write_names.py > gen/names-gen.h
+python2 scripts/write_winchance_table.py > gen/nbinom-cdf-gen.h
+python2 scripts/write_names.py > gen/names-gen.h
 
 tar -cvzf conquest.tar.gz *.cc *.h gen/*.h
